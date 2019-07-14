cdd7aeb77f773ad36ea61a8900ca83b6171a5bbc
mi-ran <azsxdc8911@gmail.com>
2019-07-14 22:42:26 +0900

    [190714] 아이폰 웹브라우저 만들기

--- miniBrowser/ViewController.h
+++ miniBrowser/ViewController.h
@@ -8,7 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate, UIWebViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *bookmargSegmentedControll;
+@property (strong, nonatomic) IBOutlet UITextField *urlTextField;
+@property (strong, nonatomic) IBOutlet UIWebView *mainWebView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityView;
+- (IBAction)bookmarkAction:(id)sender;
+- (IBAction)backAction:(id)sender;
+- (IBAction)fowardAction:(id)sender;
+- (IBAction)stopAction:(id)sender;
+- (IBAction)reflashAction:(id)sender;
 
 
 @end
--- miniBrowser/ViewController.m
+++ miniBrowser/ViewController.m
@@ -13,11 +13,54 @@
 @end
 
 @implementation ViewController
-
+@synthesize mainWebView, urlTextField, bookmargSegmentedControll;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSString *urlString = @"http://www.facebook.com";
+    [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    urlTextField.text = urlString;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString* urlString = urlTextField.text;
+    
+    if (![urlString hasPrefix:@"http://"]) {
+        urlString = [[NSString alloc]initWithFormat:@"http://%@", urlString];
+    }
+    
+    [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    [textField resignFirstResponder];
+    return YES;
+}
 
+- (IBAction)bookmarkAction:(id)sender {
+    NSString* bookmarkURL = [bookmargSegmentedControll titleForSegmentAtIndex:bookmargSegmentedControll.selectedSegmentIndex];
+    NSString* urlString = [[NSString alloc]initWithFormat:@"http://www.%@.com", bookmarkURL];
+    [mainWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    urlTextField.text = urlString;
+}
+
+- (IBAction)backAction:(id)sender {
+    [mainWebView goBack];
+}
+
+- (IBAction)fowardAction:(id)sender {
+    [mainWebView goForward];
+}
+
+- (IBAction)stopAction:(id)sender {
+    [mainWebView stopLoading];
+}
+
+- (IBAction)reflashAction:(id)sender {
+    [mainWebView reload];
+}
+
+-(void)webViewDidStartLoad:(UIWebView *)webView {
+    [_activityView startAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    [_activityView stopAnimating];
+}
 @end
